590bf24545ad65c7b6ee2eee0009faa888ad9bf3
clauscorbo <contact@corbo.no>
2026-03-12 15:23:09 +0100

    fix: serve dbt docs as static file instead of iframe page

--- site/src/app/dbt-docs/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DbtDocs() {
-  return (
-    <iframe
-      src="/dbt-docs/index.html"
-      className="h-screen w-screen border-0"
-      title="dbt Documentation"
-    />
-  );
-}
