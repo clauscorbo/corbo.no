a4adf9456effdba666c51102b8105b19c3de1b7f
clauscorbo <contact@corbo.no>
2026-03-12 09:22:42 +0100

    Add yamllint config, fix YAML formatting, update CI gateway to use config file

--- dbt/dbtproject_corbo/models/silver/sil_customers.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    DISTINCT customer_name,
-    MIN(order_date) AS first_order_date,
-    MAX(order_date) AS last_order_date,
-    COUNT(*) AS total_orders
-FROM {{ ref('bro_orders') }}
-GROUP BY customer_name
